d9d796a7a4f12c043f539d2cfb70f4032c9624ac
Karin Skans <skka19st@student.ju.se>
2019-11-07 15:20:58 +0100

    ovning 3 while-loop_version03

---
+++ ovn3_while_for/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ovn3_while_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var svar = "";
+            var antRader = 1;
+
+            // instruktion till användaren
+            Console.WriteLine("Avsluta med 'Q'");
+
+            // loop tills bokstaven Q anges
+            // testar både stor och liten bokstav
+            while ((svar != "Q") && (svar != "q"))
+            {
+                for (int ind=1; ind <= antRader ; ind++)
+                {
+                    Console.WriteLine("och sen då?");
+                } 
+
+                // antalet utskrifter dubbleras varje varv
+                antRader *= 2;
+
+                // inläsning från skärmen
+                svar = Console.ReadLine();
+            }             
+        }
+    }
+}
